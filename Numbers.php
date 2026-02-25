fbc84b422fe380b38756823e5aed6a573862f8c4
nandan0798 <nandan.as@ortusolis.com>
2026-02-25 11:56:57 +0530

    Have completed Numbers topic of Php, now moving ahead with Casting.php

---
+++ Numbers.php
@@ -0,0 +1,19 @@
+<!-- <?php
+$a = 5;
+$b = 9.9;
+$c = "99";
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+?> -->
+
+<!-- <?php
+    $x = 998.99;
+    var_dump(is_int($x));
+?> -->
+
+<?php
+$y = 7894.44;
+var_dump(is_float($y));
+?>
--- Strings.php
+++ Strings.php
@@ -1,3 +1,79 @@
 <?php
+    // $y = "Nandan";
+    // echo "Hello $y\n";
 
+        // $y = 'Nandan';
+        // echo "Hello $y";
+
+        // strlen()-> It returns the length of the string
+        // echo strlen("Nandan");
+
+        // str_word_count()-> Counts the number of words in a string 
+        // $name = "Zain";
+        // echo str_word_count($name);
+
+        // str_contains() Function -> "function checks if a string contains a specific substring".
+        // $name = "Zain is fantastic developer";
+        // var_dump(str_contains($name , "fantastic"));
+
+        // str_starts_with()->function checks if a string starts with a specific substring.
+        // $class = "I am in B.tech";
+        // var_dump(str_starts_with($class , "IN"));
+
+        // str_ends_with() Function - > function checks if a string ends with a specific substring
+        // $Family = "I have family";
+        // var_dump(str_ends_with($Family, "have"));
+
+        //Modify Strings
+        //  strtoupper() function returns a string in upper case.
+        // $x = "Hello World";
+        // echo strtoupper($x);
+
+        // strtolower() Function -> function returns a string in lower case.
+        // $name = "Nandan";
+        // echo strtolower($name);
+
+        // The str_replace() function replaces some characters with some other characters in a string.
+        // $text = "Hello Nandan";
+        // echo str_replace("Nandan", "Abhi", $text);
+
+        // The strrev() ->function reverses a string.
+        // $text = "Toxic";
+        // echo strrev($text);
+
+        // The trim() ->function removes any whitespace from the beginning or the end.
+        // $y = "Abhi Senior Php Developer        ";
+        // echo trim($y);
+
+        // Explode () - >The explode() function splits a string into an array.
+        // The first parameter of the explode() function represents the "separator". The "separator" specifies where to split the string
+        // $name = "Hi Abhi Senior Php developer";
+        // $message = explode(" ", $name);
+
+        // print_r($message);
+
+        // Concatenate Strings
+        // $x = "Abhi";
+        // $y = "Php Developer";
+        // $z = $x . $y;
+        // echo $z;
+
+        // $x = "Abhi";
+        // $y = "Php Developer";
+        // $z = $x ." " .$y;
+        // echo $z;
+
+        // substr() - Slice a String
+        // The substr() function is used to extract a part of a string (slice a string).
+        // Specify the start index and the number of characters you want to return.
+
+        // $y = "Girish Full Stack";
+        // echo substr($y,2,4);
+
+        // $z ="Wonderla Vacation";
+        // echo substr($z , 3,5);
+
+        // Escape Charecters
+        // $z = "I am Nandan \"South Indian\" from Banagalore";
+        // echo $z;
 ?>
